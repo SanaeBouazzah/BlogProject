6e5bcf9420e1a1bc11491071765845d3ff13626f
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-28 00:38:19 +0200

    BlogProject

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Astrotomic\Translatable\Contracts as TranslatableContract;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 
-class Post extends Model
+class Category extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
 }
