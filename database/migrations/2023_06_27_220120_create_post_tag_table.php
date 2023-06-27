a215c12a58945da61af232401326fb0cd4784d37
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-28 00:04:36 +0200

    BlogProject

--- database/migrations/2023_06_27_220120_create_post_tag_table.php
+++ database/migrations/2023_06_27_220120_create_post_tag_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
             $table->timestamps();
             $table->softDeletes();
         });
