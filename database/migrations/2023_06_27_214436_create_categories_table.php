3ec9503021c9d8fbaf65b00d7ef54816827758f2
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-27 23:48:23 +0200

    BlogProject

--- database/migrations/2023_06_27_214436_create_categories_table.php
+++ database/migrations/2023_06_27_214436_create_categories_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->id();
+          $table->string('image')->nullable();
+          $table->timestamps();
+          $table->softDeletes();
         });
     }
 
