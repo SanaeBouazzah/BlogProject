1af4f49310dea4ec91a3a5bf8ee3fcba0e441807
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-27 20:26:47 +0200

    BlogProject

--- database/migrations/2023_06_27_182300_create_settings_table.php
+++ database/migrations/2023_06_27_182300_create_settings_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
             $table->string('logo')->nullable();
             $table->string('favicon')->nullable();
             $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
