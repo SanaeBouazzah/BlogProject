24f181c63b2cd651f8b038b3f860efeafc8941f9
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-28 00:42:42 +0200

    BlogProject

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,4 +10,6 @@ use Astrotomic\Translatable\Translatable;
 class Category extends Model implements TranslatableContract
 {
     use HasFactory, Translatable;
+    public $translatedAttributes = ['title', 'content', 'address'];
+    protected $fillable = ['image', 'parent', 'title', 'content'];
 }
