b7e64d18ae02d5ab34b819d0c1bc9dca57a8d908
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-28 00:44:02 +0200

    BlogProject

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -11,5 +11,5 @@ class Category extends Model implements TranslatableContract
 {
     use HasFactory, Translatable;
     public $translatedAttributes = ['title', 'content', 'address'];
-    protected $fillable = ['image', 'parent', 'title', 'content'];
+    protected $fillable = [ 'logo', 'favicon', 'facebook', 'instagram', 'phone', 'email'];
 }
