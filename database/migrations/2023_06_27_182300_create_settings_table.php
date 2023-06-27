4dee26353b1f31ac7620efb342df33264bb989d1
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-27 20:24:47 +0200

    BlogProject

--- database/migrations/2023_06_27_182300_create_settings_table.php
+++ database/migrations/2023_06_27_182300_create_settings_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('logo')->nullable();
+            $table->string('favicon')->nullable();
             $table->timestamps();
         });
     }
