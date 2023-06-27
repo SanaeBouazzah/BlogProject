e2ea2edb7fcc54b01556a7131e98d3a26e161755
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-27 23:54:50 +0200

    BlogProject

--- database/migrations/2023_06_27_180550_create_posts_table.php
+++ database/migrations/2023_06_27_180550_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
           $table->id();
           $table->string('image')->nullable();
           $table->unsignedBigInteger('category_id');
+          $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
           $table->timestamps();
           $table->softDeletes();
         });
