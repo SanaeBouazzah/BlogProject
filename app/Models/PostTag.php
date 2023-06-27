c1f74740e6f8c4509519668a0fcb5f9a6ac85676
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-28 00:15:18 +0200

    BlogProject

--- app/Models/PostTag.php
+++ app/Models/PostTag.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Contracts as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 
 class PostTag extends Model
 {
