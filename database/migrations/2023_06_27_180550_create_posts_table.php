b3acc7d402749ac9e3e82f8490350d1ec527acb1
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-06-27 20:09:18 +0200

    BlogProject

--- database/migrations/2023_06_27_180550_create_posts_table.php
+++ database/migrations/2023_06_27_180550_create_posts_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->increments('id');
+          $table->integer('user_id')->nullable();
+          $table->timestamps();
+          $table->softDeletes();
         });
     }
 
