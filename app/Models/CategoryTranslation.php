a76a973882967c185602cde876ddc8956490cac2
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-02 21:02:16 +0200

    BlogProject

--- app/Models/CategoryTranslation.php
+++ app/Models/CategoryTranslation.php
@@ -4,12 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
-use Astrotomic\Translatable\Translatable;
 
-class Category extends Model implements TranslatableContract
+class Category extends Model
 {
-    use HasFactory, Translatable;
+    use HasFactory;
     public $timestamps = false;
     protected $fillable = ['title', 'content'];
 }
