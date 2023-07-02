2bd7ec58109b78891da9391947f51d738cd5cfad
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-02 22:11:31 +0200

    BlogProject

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -25,7 +25,7 @@
              گزارش گیری
           </li>
            <li class="nav-item">
-              <a class="nav-link" href="#"><i class="icon-people"></i>{{ trans('words.settings') }}</a>
+              <a class="nav-link" href="{{route('dashboard.settings')}}"><i class="icon-people"></i>{{ trans('words.settings') }}</a>
               <a class="nav-link" href="#"><i class="icon-docs"></i>  فایل ها</a>
           </li>
           <!--<li class="nav-item nav-dropdown">
