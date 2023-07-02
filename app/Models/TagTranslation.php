3f4f19e381e467647a300a68ca3be94de99046c7
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-02 21:07:22 +0200

    BlogProject

--- app/Models/TagTranslation.php
+++ app/Models/TagTranslation.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TagTranslation extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $fillable = ['tag_id', 'locale', 'title'];
 }
