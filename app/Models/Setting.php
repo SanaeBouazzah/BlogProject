87743fdc789080153674a65b4d126c8ba14c6cef
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 23:42:13 +0200

    BlogProject

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -12,4 +12,10 @@ class Setting extends Model implements TranslatableContract
     use HasFactory, Translatable;
     public $translatedAttributes = ['title', 'content', 'address'];
     protected $fillable = [ 'logo', 'favicon', 'facebook', 'instagram', 'phone', 'email', 'created_at', 'updated_at', 'deleted_at'];
+
+
+   public static function()
+   {
+    
+   }
 }
