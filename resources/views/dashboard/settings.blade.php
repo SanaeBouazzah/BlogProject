019fc99561288b33e09f405f82b9a7a0788d6cd7
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 02:22:17 +0200

    BlogProject

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -15,29 +15,29 @@
         <strong>Form</strong>
       </div>
       <div class="card-block">
-        <form action="">
+        <form action="" method="post">
           @csrf
-          <div>
+          <div class="form-group col-md-6">
             <label for="">logo</label>
             <input type="file" placeholder="logo" id="" class="form-control">
           </div>
-          <div>
+          <div class="form-group col-md-6">
             <label for="">favicon</label>
             <input type="file" placeholder="favicon" class="form-control">
           </div>
-          <div>
+          <div class="form-group col-md-6">
             <label for="">facebook</label>
             <input type="text" placeholder="facebook" class="form-control">
           </div>
-          <div>
+          <div class="form-group col-md-6">
             <label for="">instagram</label>
             <input type="text" placeholder="instagram" class="form-control">
           </div>
-          <div>
+          <div class="form-group col-md-6">
             <label for="">phone</label>
             <input type="text" placeholder="phone" class="form-control">
           </div>
-          <div>
+          <div class="form-group col-md-6">
             <label for="">email</label>
             <input type="text" placeholder="email" class="form-control">
           </div>
