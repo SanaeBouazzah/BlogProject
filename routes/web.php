459555ab05f9ac7e9aa99b1aeca65ecc0eee32e0
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 22:21:47 +0200

    BlogProject

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard.settings');
+    return view('dashboard.index');
 })->name('dashboard.index');
 
 
