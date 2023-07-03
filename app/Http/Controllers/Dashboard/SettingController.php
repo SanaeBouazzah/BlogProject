1f8b821c2a2d0414c4660d55a54210df85562925
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 22:40:02 +0200

    BlogProject

--- app/Http/Controllers/Dashboard/SettingController.php
+++ app/Http/Controllers/Dashboard/SettingController.php
@@ -11,45 +11,22 @@ class SettingController extends Controller
     {
         return view('dashboard.settings.index');
     }
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
       echo 'Hey';
     }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update()
     {
+      //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
