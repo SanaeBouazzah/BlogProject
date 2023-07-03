c0e9cb6f70ad9c34a3ebb68b29a691be2217c515
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 23:13:48 +0200

    BlogProject

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
 
-class Category extends Model implements TranslatableContract
+class Setting extends Model implements TranslatableContract
 {
     use HasFactory, Translatable;
     public $translatedAttributes = ['title', 'content', 'address'];
