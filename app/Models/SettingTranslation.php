639fecb2e28a3a77233d553729079bb2ffb0d1a4
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 23:36:08 +0200

    BlogProject

--- app/Models/SettingTranslation.php
+++ app/Models/SettingTranslation.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class SettingTranslation extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+    // public $timestamps = false;
     protected $fillable = ['setting_id', 'locale', 'title', 'content', 'address'];
 }
