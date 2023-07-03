0c37664ec8247e8c0225422a5884d06c16af65e4
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-03 02:06:07 +0200

    BlogProject

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -1,5 +1,20 @@
 @extends('dashboard.layouts.layout')
 
 @section('body')
-    <h3>hello world</h3>
+    <form action="">
+      @csrf
+      <div>
+        <label for="">logo</label>
+        <input type="text" placeholder="logo">
+      </div>
+      <div>
+        <label for="">favicon</label>
+        <input type="text" placeholder="favicon">
+      </div>
+      <div>
+        <label for="">facebook</label>
+        <input type="text" placeholder="facebook">
+      </div>
+    BlogP
+    </form>
 @endsection
