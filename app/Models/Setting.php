37c8f838dcec750d63269c8389c9cc53437e9a18
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-06 17:49:36 +0200

    BlogProject

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\SettingTranslation;
 use Illuminate\Database\Eloquent\Model;
-use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
 
 class Setting extends Model implements TranslatableContract
 {
