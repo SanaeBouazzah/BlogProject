7c14751d9157706f4869f9d1dde40d1466aed971
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2023-07-15 18:49:45 +0200

    BlogProject

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -13,18 +13,19 @@ class Setting extends Model implements TranslatableContract
     use HasFactory, Translatable;
     public $translatedAttributes = ['title', 'content', 'address'];
     protected $fillable = [ 'id','logo', 'favicon', 'facebook', 'instagram', 'phone', 'email', 'created_at', 'updated_at', 'deleted_at'];
-   public static function checkSettings()
-   {
-      $settings =Self::all();
-      if(count($settings)<1){
-        $data = [
-          'id' => 1,
-        ];
-        foreach (config('app.languages') as $key => $value) {
-          $data[$key]['title'] = $value;
-        }
-        Self::create($data);
-      }
-      return Self::first();
+    public static function checkSettings()
+    {
+       $settings = Self::all();
+       if (count($settings) < 1) {
+           $data = [
+               'id' => 1,
+           ];
+           foreach (config('app.languages') as $key => $value) {
+               $data[$key]['title'] = $value;
+           }
+           Self::create($data);
+       }
+
+       return Self::first();
    }
 }
